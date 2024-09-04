756cfb070c92993ad9759702cade061a0bb5a7a1
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-09-04 10:49:48 +0700

    Ss

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SUMDUONGCHEO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int n;
+            int sum=0;
+            int[,] arr1= new int[10,10];
+
+            Console.WriteLine("Nhap size ma tran: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap cac phan tu cua ma tran:\n");
+            for(a=0;a<n;a++)
+            {
+                for(b=0;b<n;b++)
+                {
+                    Console.Write("Phan tu[{0}],[{1}]: ", a, b);
+                    arr1[a, b] = int.Parse(Console.ReadLine());
+                    if(a==b) sum = sum + arr1[a,b];
+                }
+            }
+            Console.Write("In ma tran:\n");
+            for(a=0;a<n;a++)
+            {
+                for(b=0;b<n;b++)
+                {
+                    Console.Write("{0} ", arr1[a, b]);
+                Console.Write("\n");
+                }
+            }
+            Console.Write("Tong cac phan tu tren duong cheo chinh la: {0}\n", sum);
+            Console.ReadKey();
+        }
+    }
+}
